44cd7d58f7ce14efcbcb5ac3f0bcc1ae577d2d6f
unknown <daocongkha.mochi@gmail.com>
2024-01-17 08:03:27 +0700

    delete logic ssl

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,15 @@
 import express, { Request } from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import http from "http";
-import fs from "fs";
 import bodyParser from "body-parser";
 import usersRouter from "./routes/users.routes";
 import datanaseServices from "~/services/database.services";
 import storyRouter from "./routes/storys.routes";
 import cors from "cors";
 
-import https from "https";
-import path from "path";
-
 datanaseServices.connect();
 
 const app = express();
-
-const sslServer = https.createServer(
-  {
-    key: fs.readFileSync(path.join(__dirname, "config/cetificates/key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "config/cetificates/cert.pem")),
-  },
-  app,
-);
 
 app.use(cors<Request>());
 app.use(express.json({ limit: "500mb" }));
@@ -43,6 +30,6 @@ const port = 5000;
 app.use("/user", usersRouter);
 app.use("/storys", storyRouter);
 
-sslServer.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`App server listening on port ${port}`);
 });
